02dfb373027cf16cc95fe7bfd6d332d4aee3896c
BryleG <gregoriobryle05@gmail.com>
2023-03-20 07:48:05 +0800

    server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
 
       // SAVE USER TO MONGODB...
       if (profileObj) {
-        const response = await fetch ('http://localhost:8080/api/v1/users', {
+        const response = await fetch ('https://apex-estate-e2kh.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json'},
           body: JSON.stringify ({
@@ -126,7 +126,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://apex-estate-e2kh.onrender.com")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
@@ -145,16 +145,7 @@ function App() {
               show: AgentProfile,
               icon: <PeopleAltOutlined/>
             },
-            // {
-            //   name: "reviews",
-            //   list: Home,
-            //   icon: <StarOutlineRounded/>
-            // },
-            // {
-            //   name: "messages",
-            //   list: Home,
-            //   icon: <ChatBubbleOutline/>
-            // },
+           
             {
               name: "my-profile",
               options: { label: 'My Profile' },
--- client/src/components/layout/header/index.tsx
+++ client/src/components/layout/header/index.tsx
@@ -29,13 +29,6 @@ export const Header: React.FC = () => {
           justifyContent="flex-end"
           alignItems="center"
         >
-          {/* <IconButton
-            onClick={() => {
-              setMode();
-            }}
-          >
-            {mode === "dark" ? <LightModeOutlined /> : <DarkModeOutlined />}
-          </IconButton> */}
           <Stack
             direction="row"
             gap="16px"
